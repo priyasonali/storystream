c81c7631c850caaf2f0c9ef2b49cec87dd1c84a3
Sonali Priya <sonalipriya@outlook.com>
2016-12-06 18:16:45 -0600

    Database is made

---
+++ class.database.php
@@ -0,0 +1,22 @@
+
+<?php
+  
+  class Database {
+	
+	protected $db_name = 'storystream';
+	protected $db_user = 'root';
+	protected $db_pass = '';
+	protected $db_host = 'localhost';
+	
+	public function connect() {
+	
+		$mysqli = new mysqli( $this->db_host, $this->db_user, $this->db_pass, $this->db_name );
+		
+		if ($mysqli->connect_error) {
+      return false;
+    } else {
+      return $mysqli;
+    }
+	}
+
+}
