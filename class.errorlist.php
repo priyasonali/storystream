b01da6937d68a788cb98b5f5b937a66c66f740c3
Sonali Priya <sonalipriya@outlook.com>
2016-12-08 16:50:51 -0600

    Sign up api implemented

---
+++ class.errorlist.php
@@ -0,0 +1,11 @@
+
+<?php
+  
+  class ErrorList {
+      public function __construct(){
+          
+         $this->errlist = array();
+          $this->errlist[0] = "This email is already registered.";
+          $this->errlist[1] = "This username is taken.";
+      }
+}
--- registration.php
+++ registration.php
@@ -3,6 +3,7 @@
 // Project: Story Stream
     header("Content-type: application/json");
     require 'class.database.php';
+    require 'class.errorlist.php';
     $response = array();
     
 
@@ -19,19 +20,31 @@
         public function pushdata(){  
             $db = new Database;
             $mysqli = $db->connect();
-
+            $errnum = new ErrorList;
+            
             //if(!$mysqli) echo "not connected"; else echo "connected";
+            $result = $mysqli->query("SELECT * FROM users WHERE user_email='$this->user_email'")->num_rows;
+            if($result>0)
+             { 
+                $response["error"]["err_code"] = 0;  
+                $response["error"]["err_desc"] = $errnum->errlist[0];                
+             } 
+             else if(($mysqli->query("SELECT * FROM users WHERE user_name='$this->user_name'")->num_rows)>0){
+                $response["error"]["err_code"] = 1;  
+                $response["error"]["err_desc"] = $errnum->errlist[1];    
+             }
+             else           
+             {
+                 if($result = $mysqli->query("INSERT INTO users (user_email, user_name, user_pass, user_status) VALUES ('$this->user_email', '$this->user_name', '$this->user_pass', 'Pending')"))
+                {
+                    $response["status"] = "Success"; 
+                } else { 
+                    $response["status"] = "Failed"; 
+                }
+             }
              
-            if($result = $mysqli->query("INSERT INTO users (user_email, user_name, user_pass, user_status) VALUES ('$this->user_email', '$this->user_name', '$this->user_pass', 'Pending')"))
-            {
-                $response["status"] = "Success"; 
-            }
-            else                
-            { 
-                $response["status"] = "Failed"; 
-            }
-            $mysqli -> close();
-            return json_encode($response, JSON_PRETTY_PRINT);
+                $mysqli -> close();
+                return json_encode($response, JSON_PRETTY_PRINT);
         }
         
     }
